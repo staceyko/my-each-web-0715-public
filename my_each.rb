be030d1949ce252a1d279d2d6a4fda5c14296c34
staceyko <ko.stacey@gmail.com>
2015-08-01 17:18:22 -0400

    done

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+    i = 0
+    while i < array.length
+        yield array[i]
+        i += 1
+    end
+    array
 end
+
+#def my_each(array)
+#    i = 0
+#    while i < array.length
+#        puts "#{array[i]}"
+#    i += 1
+#    end
+#    array
+#end
